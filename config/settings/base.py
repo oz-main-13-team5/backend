c760bc837adf9c7708c723807ad88692efb13ef9
TeaCat-Neya <TeaCat0416@gmail.com>
2025-10-27 18:14:23 +0900

    🐛 Update settings
    base.py and dev.py

--- config/settings/base.py
+++ config/settings/base.py
@@ -22,3 +22,42 @@ DATABASES = {
         'PORT': os.getenv("DB_PORT"),
     }
 }
+
+INSTALLED_APPS = [
+    'django.contrib.admin',          # admin
+    'django.contrib.auth',           # 사용자 인증
+    'django.contrib.contenttypes',   # 모델 타입 관련
+    'django.contrib.sessions',       # 세션
+    'django.contrib.messages',       # 메시지 프레임워크
+    'django.contrib.staticfiles',    # static 파일 처리
+
+    # 프로젝트 앱
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'templates'],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+STATIC_URL = '/static/'
