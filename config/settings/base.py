8ea77715d41c7bed1f2bf0d6fc511d65ee2ed90e
TeaCat-Neya <TeaCat0416@gmail.com>
2025-10-27 15:54:34 +0900

    🌱 INSTALLED_APPS = DJANGO_APPS + MY+APPS
    settings/base.py

--- config/settings/base.py
+++ config/settings/base.py
@@ -29,8 +29,7 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-
-INSTALLED_APPS = [
+DJANGO_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+MY_APPS = [
+    '',
+]
+
+INSTALLED_APPS = DJANGO_APPS + MY_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
