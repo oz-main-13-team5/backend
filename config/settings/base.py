0fbc5098e37b1f4661d555d4a3719a8753ceab7d
TeaCat-Neya <TeaCat0416@gmail.com>
2025-10-28 15:03:33 +0900

    🌱 Divide INSTALLED_APPS
    DJANGO APPS + MY APPS

--- config/settings/base.py
+++ config/settings/base.py
@@ -3,7 +3,6 @@ import datetime
 from pathlib import Path
 from dotenv import load_dotenv
 
-
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 env_file = os.getenv("ENV_FILE", ".env.dev")
