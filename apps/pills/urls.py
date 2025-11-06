ab24f9c6dd01975110919668eee39a0dcaa04a12
BH13KDR <bh13kdr@example.com>
2025-11-06 14:42:36 +0900

    🐛 fix: 검색어가 2글자 미만일 경우 400 에러 반환 🐛 fix: page 파라미터가 1 이상의 정수가 아닐 경우 400 에러 반환 🔍 refactor: Query Params 방식으로 검색어 처리 로직 개선

--- apps/pills/urls.py
+++ apps/pills/urls.py
@@ -6,9 +6,5 @@ from apps.pills.views.pill_search_view import PillSearchView
 urlpatterns = [
     path("page/<int:page>/", PillListView.as_view(), name="pill-list"),
     path("<str:item_seq>/", PillDetailView.as_view(), name="pill-detail"),
-    path(
-        "search/<str:keyword>/page/<int:page>/",
-        PillSearchView.as_view(),
-        name="pill-search",
-    ),
+    path('search/', PillSearchView.as_view(), name='pill-search'),
 ]
