a7371121223109cfcdc63d069341cbc8442b1ae2
codfin02 <codfin02@gmail.com>
2025-11-06 12:43:47 +0900

    test: 북마크·마이페이지 테스트 추가 및 설정 보완

--- apps/users/models/user.py
+++ apps/users/models/user.py
@@ -29,6 +29,7 @@ class UserManager(BaseUserManager):
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.is_active = False
+        user.last_login = timezone.now()
         user.save(using=self._db)
         return user
 
