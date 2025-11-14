1cf167d69c41a5cbc168b826dfc50679faefdc9e
badatga <widdefinesme@gmail.com>
2025-11-14 16:36:26 +0900

    🔧 chore: 프로덕션 설정 및 Gunicorn 스크립트 업데이트
    * `ALLOWED_HOSTS` 및 `CSRF_TRUSTED_ORIGINS` 설정 추가
    * Gunicorn 바인드 주소를 `127.0.0.1`로 수정

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,7 +1,16 @@
 from .base import *
 
-ALLOWED_HOSTS = ["mydomain.com"]
 DEBUG = False
+
+ALLOWED_HOSTS = [
+    "ec2-13-209-193-43.ap-northeast-2.compute.amazonaws.com",
+    "mydomain.com",
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://ec2-13-209-193-43.ap-northeast-2.compute.amazonaws.com",
+    "http://mydomain.com",
+]
 
 ROOT_URLCONF = "config.urls"
 WSGI_APPLICATION = "config.wsgi.application"
