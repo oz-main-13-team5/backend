cb5288878fef1e64b496b2ed47754b85fafb2fd1
badatga <widdefinesme@gmail.com>
2025-11-14 17:26:38 +0900

    fix/settings-prod (#29)
    * 🎉 chore: 프로덕션용 Gunicorn 배포 스크립트 추가
    
    프로덕션 환경에서 Gunicorn 실행을 위한 run_gunicorn_prod.sh 스크립트 추가
    
    환경별 변수를 관리하기 위한 .env.prod 포함
    
    * 🔧 chore: 프로덕션 환경 설정 업데이트 (Django 설정 추가)
    
    * `ROOT_URLCONF`, `WSGI_APPLICATION` 등 필수 설정 추가
    * 언어 및 시간대 설정 (`LANGUAGE_CODE`, `TIME_ZONE`) 업데이트
    * `DEFAULT_AUTO_FIELD` 기본값 설정 적용
    
    * 🔧 chore: 프로덕션 설정 및 Gunicorn 스크립트 업데이트
    
    * `ALLOWED_HOSTS` 및 `CSRF_TRUSTED_ORIGINS` 설정 추가
    * Gunicorn 바인드 주소를 `127.0.0.1`로 수정
    
    * 🔧 chore: Gunicorn 프로덕션 스크립트 데몬 모드 및 로그 레벨 설정 추가
    
    * Gunicorn 실행을 데몬 모드로 변경
    * 로그 레벨을 `info`로 설정하여 실행 로그 개선

--- deploy/run_gunicorn_prod.sh
+++ deploy/run_gunicorn_prod.sh
@@ -10,7 +10,9 @@ source .venv/bin/activate
 # prod 환경 변수 파일 지정
 export ENV_FILE="envs/.env.prod"
 
-# 워커 수 3 - 임의 지정상태
-exec gunicorn config.wsgi:application \
+# 백그라운드(데몬) 실행, 로그 파일 저장 안 함
+gunicorn config.wsgi:application \
   --bind 127.0.0.1:8000 \
-  --workers 3
+  --workers 3 \
+  --log-level info \
+  --daemon
