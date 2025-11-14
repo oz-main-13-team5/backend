c656d6418ca2e47f9d48eb50c54b0a56387daa13
badatga <widdefinesme@gmail.com>
2025-11-14 16:26:07 +0900

    🔧 chore: 프로덕션 환경 설정 업데이트 (Django 설정 추가)
    * `ROOT_URLCONF`, `WSGI_APPLICATION` 등 필수 설정 추가
    * 언어 및 시간대 설정 (`LANGUAGE_CODE`, `TIME_ZONE`) 업데이트
    * `DEFAULT_AUTO_FIELD` 기본값 설정 적용

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -2,3 +2,13 @@ from .base import *
 
 ALLOWED_HOSTS = ["mydomain.com"]
 DEBUG = False
+
+ROOT_URLCONF = "config.urls"
+WSGI_APPLICATION = "config.wsgi.application"
+
+LANGUAGE_CODE = "ko-kr"
+TIME_ZONE = "Asia/Seoul"
+USE_I18N = True
+USE_TZ = True
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
