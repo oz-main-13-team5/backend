dad1059236b2ab1a5b1873d2427fed83cfa71299
BH13KDR <bh13kdr@example.com>
2025-11-18 18:04:05 +0900

    token_refresh를 고쳤습니다.

--- apps/users/views/auth/login_view.py
+++ apps/users/views/auth/login_view.py
@@ -93,7 +93,7 @@ class LoginView(APIView):
             key="refresh_token",
             value=token_pair["refresh"],
             httponly=True,
-            secure=True,
+            secure=False,
             samesite="Lax",
             max_age=int(refresh_lifetime.total_seconds()),
         )
